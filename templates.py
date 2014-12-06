0e552fdc7fd8c89c34fdbf758f07f3d99a2e992f
Zhen <zhenw.wang@gmail.com>
2014-12-05 21:50:01 -0500

    md5

--- templates.py
+++ templates.py
@@ -6,6 +6,18 @@ from google.appengine.ext import db
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
+import hashlib
+def hash_str(s):
+    return hashlib.md5(s).hexdigest()
+
+def make_secure_val(s):
+    return "%s|%s" % (s, hash_str(s)) #use | instead of ,
+
+def check_secure_val(h):
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+        return val
+
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -18,17 +30,21 @@ class Handler(webapp2.RequestHandler):
 class MainPage(Handler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        visits = self.request.cookies.get('visits', '0')
+        visits = 0
+        visit_cookie_str = self.request.cookies.get('visits')
+        if visit_cookie_str:
+            cookie_val = check_secure_val(visit_cookie_str)
+            if cookie_val:
+                visits = int(cookie_val)
 
-        if visits.isdigit():
-            visits = int(visits) + 1
-        else:
-            visits = 0
+        visits += 1
+
+        new_cookie_val = make_secure_val(str(visits))
+        self.response.headers.add_header('Set-Cookie', 'visits=%s' % new_cookie_val)
 
         if visits > 10:
             self.write("good ")
         else:
-            self.response.headers.add_header('Set-Cookie', 'visits=%s' % visits)
-        self.write("%s times" % visits)
+            self.write("%s times" % visits)
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
